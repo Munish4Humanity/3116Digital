f4d5816c1db3876472548ffdb6a47e7a0ee76713
Munish <munish4humanity@gmail.com>
2020-07-29 00:14:02 +0530

    Search Commit

---
+++ digital-SPA/src/app/_resources/home/home.component.ts
@@ -0,0 +1,43 @@
+import { Component, OnInit } from "@angular/core";
+import { FormControl } from "@angular/forms";
+interface UPC {
+  value: string;
+  viewValue: string;
+}
+interface Operator {
+  value: string;
+  viewValue: string;
+}
+@Component({
+  selector: "app-home",
+  templateUrl: "./home.component.html",
+  styleUrls: ["./home.component.css"],
+})
+export class HomeComponent implements OnInit {
+  options: string[] = ["One", "Two", "Three"];
+  upc: UPC[] = [
+    { value: "1", viewValue: "UAC" },
+    { value: "2", viewValue: "Asin" },
+    { value: "3", viewValue: "Location" },
+    { value: "3", viewValue: "Sku" },
+    { value: "3", viewValue: "Price" },
+    { value: "3", viewValue: "Qty(On Hand)" },
+    { value: "3", viewValue: "Qty(Avail to list)" },
+    { value: "3", viewValue: "Last Modified " },
+  ];
+  operator: Operator[] = [
+    { value: "1", viewValue: "Equal" },
+    { value: "1", viewValue: "Not Equal" },
+    { value: "1", viewValue: "Contains" },
+    { value: "1", viewValue: "Not Contains" },
+  ];
+  dataSource = [];
+  constructor() {}
+
+  ngOnInit() {}
+  onAddData() {
+    this.dataSource.push(this.dataSource.length);
+  }
+
+  runQuery() {}
+}
